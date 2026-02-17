058e4e6b0d9876974e579eab3abd451bbaaf2dee
PascalKon <118291843+PascalKon@users.noreply.github.com>
2026-02-17 22:49:24 +0200

    Add files via upload

---
+++ flows/genus/Genus_Results/Test/Genus_Reports/Rtl_Gen_LEC/rtl_genv_db/lec.cmd
@@ -0,0 +1,64 @@
+SET FLatten Model -verification_info_strip_cdn_name_extension
+REAd IMplementation Information /home/p/paschalk/Desktop/I2C_Memory/flows/genus/Genus_Results/Test/Genus_Reports/fv/\
+   -revised genv
+SET PARAllel Option -threads 1,4 -norelease_license
+SET COmpare Options -threads 1,4
+SET MUltiplier Implementation boothrca -both
+SET UNDEfined Cell black_box -noascend -both
+ADD SEarch Path /mnt/apps/prebuilt/eda/designkits/xfab/XKIT/x_all/cadence/XFAB_AMS_RefKit-cadence_IC61/v2_5_1/pdk/xh018/diglibs/D_CELLS_HD/v3_0/liberty_LPMOS/v3_0_0/PVT_1_80V_range/\
+   /mnt/apps/prebuilt/eda/designkits/xfab/XKIT/x_all/cadence/XFAB_AMS_RefKit-cadence_IC61/v2_5_1/pdk/xh018/diglibs/IO_CELLS_3V/v2_1/liberty_LPMOS/v2_1_0/PVT_1_80V_3_30V_range/\
+   -library -both
+REAd LIbrary -liberty -both /mnt/apps/prebuilt/eda/designkits/xfab/XKIT/x_all/cadence/XFAB_AMS_RefKit-cadence_IC61/v2_5_1/pdk/xh018/diglibs/D_CELLS_HD/v3_0/liberty_LPMOS/v3_0_0/PVT_1_80V_range/D_CELLS_HD_LPMOS_fast_1_98V_m40C.lib\
+   /mnt/apps/prebuilt/eda/designkits/xfab/XKIT/x_all/cadence/XFAB_AMS_RefKit-cadence_IC61/v2_5_1/pdk/xh018/diglibs/IO_CELLS_3V/v2_1/liberty_LPMOS/v2_1_0/PVT_1_80V_3_30V_range/IO_CELLS_3V_LPMOS_fast_1_98V_3_60V_m40C.lib\
+   /mnt/apps/prebuilt/eda/designkits/xfab/XKIT/x_all/cadence/XFAB_AMS_RefKit-cadence_IC61/v2_5_1/pdk/xh018/diglibs/D_CELLS_HD/v3_0/liberty_LPMOS/v3_0_0/PVT_1_80V_range/D_CELLS_HD_LPMOS_typ_1_80V_25C.lib\
+   /mnt/apps/prebuilt/eda/designkits/xfab/XKIT/x_all/cadence/XFAB_AMS_RefKit-cadence_IC61/v2_5_1/pdk/xh018/diglibs/IO_CELLS_3V/v2_1/liberty_LPMOS/v2_1_0/PVT_1_80V_3_30V_range/IO_CELLS_3V_LPMOS_typ_1_80V_3_30V_25C.lib\
+   /mnt/apps/prebuilt/eda/designkits/xfab/XKIT/x_all/cadence/XFAB_AMS_RefKit-cadence_IC61/v2_5_1/pdk/xh018/diglibs/D_CELLS_HD/v3_0/liberty_LPMOS/v3_0_0/PVT_1_80V_range/D_CELLS_HD_LPMOS_slow_1_62V_175C.lib\
+   /mnt/apps/prebuilt/eda/designkits/xfab/XKIT/x_all/cadence/XFAB_AMS_RefKit-cadence_IC61/v2_5_1/pdk/xh018/diglibs/IO_CELLS_3V/v2_1/liberty_LPMOS/v2_1_0/PVT_1_80V_3_30V_range/IO_CELLS_3V_LPMOS_slow_1_62V_3_00V_175C.lib
+SET UNDRiven Signal 0 -golden
+SET NAming Style genus -golden
+SET NAming Rule %s[%d] -instance_array -golden
+SET NAming Rule %s_reg -register -golden
+SET NAming Rule %L.%s %L[%d].%s %s -instance -golden
+SET NAming Rule %L.%s %L[%d].%s %s -variable -golden
+SET NAming Rule -ungroup_separator _ -golden
+SET HDl Options -const_port_extend
+SET HDl Options -unsigned_conversion_overflow on
+SET HDl Options -v_to_vd on
+SET HDl Options -VERILOG_INCLUDE_DIR sep:src
+ADD SEarch Path . -design -golden
+REAd DEsign -enumconstraint -define SYNTHESIS -merge bbox -golden -lastmod -noelab -sv09 /home/p/paschalk/Desktop/I2C_Memory/I2C_Memory_UVM/src/I2C_Memory.sv\
+   /home/p/paschalk/Desktop/I2C_Memory/I2C_Memory_UVM/src/I2C_Slave.sv /home/p/paschalk/Desktop/I2C_Memory/I2C_Memory_UVM/src/Memory.sv
+ELAborate DEsign -golden -root I2CAndMemory -rootonly
+REAd DEsign -verilog95 -revised -lastmod -noelab gen.v
+ELAborate DEsign -revised -root I2CAndMemory
+UNIQuify -all -nolib -golden
+REPort DEsign Data
+REPort BLack Box
+SET FLatten Model -seq_constant
+SET FLatten Model -seq_constant_x_to 0
+SET FLatten Model -nodff_to_dlat_zero
+SET FLatten Model -nodff_to_dlat_feedback
+SET FLatten Model -hier_seq_merge
+SET FLatten Model -balanced_modeling
+ADD PIn Constraints 0 se -revised
+ADD PIn Constraints 0 test_mode -revised
+ADD IGnored Outputs scan_out -revised
+CHEck VErification Information
+SET ANalyze Option -auto -report_map
+WRIte HIer_compare Dofile hier_tmp2.lec.do -verbose -noexact_pin_match -constraint -usage -replace -balanced_extraction\
+   -input_output_pin_equivalence -prepend_string "report_design_data; report_unmapped_points -summary; report_unmapped_points -notmapped; analyze_datapath -module -verbose; eval analyze_datapath -flowgraph -verbose"
+RUN HIer_compare hier_tmp2.lec.do -dynamic_hierarchy
+REPort HIer_compare Result -dynamicflattened
+REPort VErification -hier -verbose
+SET SYstem Mode lec
+WRIte COmpared Points noneq.compared_points.I2CAndMemory.rtl.genv.tcl -class noneq -tclmode -replace
+ANAlyze NOnequivalent -source_diagnosis
+REPort NOnequivalent Analysis
+REPort TEst Vector -noneq
+SET SYstem Mode setup
+WRIte VErification Information
+REPort VErification Information
+REPort IMplementation Information
+SET SYstem Mode lec
+ANAlyze RESults -logfiles rtl_gen.lec.log
+EXIt -f
