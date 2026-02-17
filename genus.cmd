f12942ae59bd1bbe533aac5ec15752c85cb95d88
PascalKon <118291843+PascalKon@users.noreply.github.com>
2026-02-17 22:25:53 +0200

    Delete genus.cmd

--- genus.cmd
+++
@@ -1,7 +0,0 @@
-# Cadence Genus(TM) Synthesis Solution, Version 22.13-s093_1, built Sep  5 2023 17:56:02
-
-# Date: Sat Nov 29 15:37:47 2025
-# Host: cn97.it.auth.gr (x86_64 w/Linux 5.14.0-570.52.1.el9_6.x86_64) (12cores*12cpus*1physical cpu*Intel(R) Xeon(R) Gold 6230 CPU @ 2.10GHz 28160KB)
-# OS:   Rocky Linux release 9.6 (Blue Onyx)
-
-source TCLs/genus_test.tcl
