2861b01210fb4a4040e5117195d0040478848af7
PascalKon <118291843+PascalKon@users.noreply.github.com>
2026-02-17 22:03:54 +0200

    Add files via upload

---
+++ genus_mmmc.cmd
@@ -0,0 +1,7 @@
+# Cadence Genus(TM) Synthesis Solution, Version 21.15-s080_1, built Sep 23 2022 12:57:55
+
+# Date: Tue Nov 25 22:22:38 2025
+# Host: cn88.it.auth.gr (x86_64 w/Linux 5.14.0-570.52.1.el9_6.x86_64) (12cores*12cpus*1physical cpu*Intel(R) Xeon(R) Gold 6230 CPU @ 2.10GHz 28160KB)
+# OS:   Rocky Linux release 9.6 (Blue Onyx)
+
+source TCLs/genus_mmmc.tcl
