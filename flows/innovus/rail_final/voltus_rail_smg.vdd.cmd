51243ad5af4a00f487c02dcda5779ba8a34118b5
PascalKon <118291843+PascalKon@users.noreply.github.com>
2026-02-18 21:59:52 +0200

    Add files via upload

---
+++ flows/innovus/rail_final/voltus_rail_smg.vdd.cmd
@@ -0,0 +1,24 @@
+{
+    "nets": {
+        "vdd": {
+            "name": "vdd",
+            "output_path": ".\/rail_final\/vdd_25C_avg_1",
+            "nominal_voltage": "1.79999995"
+        }
+    },
+    "cellview_file": ".\/work\/vdd.cell.views",
+    "technology_file": "Libs\/QRS_PVS\/Max\/qrcTechFile",
+    "temporary_directory": "\/tmp\/innovus_temp_146358_cn89.it.auth.gr_paschalk_LseSCo",
+    "pgdb_path": ".\/work\/",
+    "use_def_pins": "true",
+    "sfedb_path": ".\/work\/SFEDB",
+    "thread_num": "1",
+    "cell_lib_path": ".\/rail_final\/era_work\/incremental_output0\/pgv\/techonly.cl",
+    "temperature": "25",
+    "accuracy_mode": "xd",
+    "extraction_backend": "zx",
+    "disallow_tolerance": "false",
+    "disallow_via_tolerance": "true",
+    "tolerance_variated_side": "2",
+    "tolerance_fixed_side": "1"
+}
